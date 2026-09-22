c342d979a8b81fbb2c99cfd8478e31b42f4c6337
adrianacuevasleon07-svg <rollordstv@gmail.com>
2026-09-22 16:41:44 -0400

    cambios

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,29 +2,50 @@
 #Registro de estudiantes y calificaciones
 #Diseña un sistema donde se defina un diccionario para un curso. Primero solicita el nombre del curso y luego permite agregar los nombres de los alumnos en una lista. El ingreso de alumnos debe detenerse únicamente cuando el usuario presione la tecla "q". Al salir, imprime la lista completa en orden.
 
-curso = {}
-
-#nombre del curso
 nombre_curso = input("Ingresa el nombre del curso: ")
-curso["nombre_curso"] = nombre_curso
-curso["estudiantes"] = []
 
-print("\nIngresa los nombres de los estudiantes (presiona 'q' para finalizar):")
+curso = {
+    "nombre": nombre_curso,
+    "alumnos": [],
+    "notas": []
+}
+
+print("\n--- Registro de Alumnos ---")
+print("Ingresa los nombres de los alumnos (Escribe 'q' para finalizar):")
 
 while True:
-    nombre = input("Nombre del estudiante: ").strip()
+    nombre_alumno = input("Nombre del alumno: ").strip()
     
-    if nombre.lower() == 'q':
+    if nombre_alumno.lower() == 'q':
         break
     
-    if nombre:
-        curso["estudiantes"].append(nombre)
+    if nombre_alumno:
+        calificaciones= {
+            'matematica' : '',
+            'quimica' : '',
+            'fisica' : '',
+            'ingles' : '',
+            'geografia' : ''
+        }
+        alumno_notas = []
+        for materia in calificaciones:
+            nota = int(input(f'¿Cuál fue la calificación de {nombre_alumno} en {materia}?: '))
+            calificaciones[materia] = nota
+            alumno_notas.append(nota)
 
-curso["estudiantes"].sort()
+        alumno_datos = {
+            "nombre": nombre_alumno,
+            "calificaciones": calificaciones
+        }
+        suma = sum(alumno_notas)
+        total = suma / len(alumno_notas)
+        curso["alumnos"].append(alumno_datos)
+        curso["notas"].append(total)
 
-print(f"\nCURSO: {curso['nombre_curso']}")
-print("="*30)
-print("Lista de estudiantes ordenada:")
 
-for i, estudiante in enumerate(curso["estudiantes"], 1):
-    print(f"{i}. {estudiante}")
+print("\n" + "="*30)
+print(f"Curso: {curso['nombre']}")
+print(f"Total de alumnos registrados: {len(curso['alumnos'])}")
+print("Lista de alumnos en orden alfabético:")
+for i, alumno in enumerate(curso["alumnos"], start=1):
+    print(f"{i}. {alumno}")
